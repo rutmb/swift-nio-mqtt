06e057c1eb897733f01d7bf7f23ed57f98d297ce
Igor <tamboff69@gmail.com>
2020-07-26 19:49:06 +0300

    feat: changed a source of the swift-nio-transport-service

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.2.1"),
+        .package(url: "https://github.com/rutmb/swift-nio-transport-services", from: "1.7.0"),
     ],
     targets: [
         .target(name: "MQTTCodec", dependencies: ["NIO", "NIOFoundationCompat"]),
